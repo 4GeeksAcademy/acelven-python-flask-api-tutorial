b401a72472b49eb4e6622c48bae717d6a419faf1
Acelven <124097190+Acelven@users.noreply.github.com>
2023-05-11 04:01:50 +0000

    Completed

--- src/app.py
+++ src/app.py
@@ -12,10 +12,19 @@ def hello_world():
     return json_text
 
 @app.route('/todos', methods=['POST'])
-def add_new_todo():
-    request_body = request.data
+def add_new_todo(): 
+    request_body = request.json
     print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
+    todos.append(request_body)
+    
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    todos.pop((position-1))
+    
+    return jsonify(todos)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
